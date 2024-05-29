6df084345f07d8f9b30724e1c8e1109d9f67eef0
dmitri.candu <d.candu@hotmail.com>
2024-05-29 20:03:25 +0300

    add tests

--- s3-package-example/test/fancier-s3-bucket.test.ts
+++ s3-package-example/test/fancier-s3-bucket.test.ts
@@ -1,18 +1,124 @@
 import { Stack } from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
+import { Capture, Template } from 'aws-cdk-lib/assertions';
 import { FancierS3Bucket } from '../src';
 
 describe('FancierS3Bucket', () => {
-  it('creates an S3 bucket', () => {
+  let template: Template;
+
+  beforeEach(() => {
     const stack = new Stack();
 
     new FancierS3Bucket(stack, 'FancierS3Bucket', {});
 
-    const template = Template.fromStack(stack);
+    template = Template.fromStack(stack);
+  });
 
+  it('creates an S3 bucket', () => {
     template.resourceCountIs('AWS::S3::Bucket', 1);
 
     // Inspecting the template:
     // console.log(JSON.stringify(template.toJSON()));
   });
+
+  it('enables encryption on the S3 bucket', () => {
+    template.hasResourceProperties('AWS::S3::Bucket', {
+      BucketEncryption: {
+        ServerSideEncryptionConfiguration: [{
+          ServerSideEncryptionByDefault: {
+            SSEAlgorithm: 'AES256',
+          },
+        }],
+      },
+    });
+  });
+
+  it('has an update/replace and deletion policy of retain', () => {
+    template.hasResource('AWS::S3::Bucket', {
+      UpdateReplacePolicy: 'Retain',
+      DeletionPolicy: 'Retain',
+    });
+  });
+
+  it('denies all requests which are not secure', () => {
+    const bucketRefA = new Capture();
+    const bucketRefB = new Capture();
+
+    template.hasResourceProperties('AWS::S3::BucketPolicy', {
+      PolicyDocument: {
+        Statement: [
+          {
+            Action: 's3:*',
+            Condition: {
+              Bool: {
+                'aws:SecureTransport': 'false',
+              },
+            },
+            Effect: 'Deny',
+            Principal: {
+              AWS: '*',
+            },
+            Resource: [
+              {
+                'Fn::GetAtt': [
+                  // Match.anyValue(),
+                  bucketRefA,
+                  'Arn',
+                ],
+              },
+              {
+                'Fn::Join': [
+                  '',
+                  [
+                    {
+                      'Fn::GetAtt': [
+                        // Match.anyValue(),
+                        bucketRefB,
+                        'Arn',
+                      ],
+                    },
+                    '/*',
+                  ],
+                ],
+              },
+            ],
+          },
+        ],
+        Version: '2012-10-17',
+      },
+    });
+
+    expect(bucketRefA.asString()).toEqual(bucketRefB.asString());
+  });
+
+  // Before refactoring:
+  // it('creates an S3 bucket', () => {
+  //   const stack = new Stack();
+  //
+  //   new FancierS3Bucket(stack, 'FancierS3Bucket', {});
+  //
+  //   const template = Template.fromStack(stack);
+  //
+  //   template.resourceCountIs('AWS::S3::Bucket', 1);
+  //
+  //   // Inspecting the template:
+  //   // console.log(JSON.stringify(template.toJSON()));
+  // });
+  //
+  // it('enables encryption on the S3 bucket', () => {
+  //   const stack = new Stack();
+  //
+  //   new FancierS3Bucket(stack, 'FancierS3Bucket', {});
+  //
+  //   const template = Template.fromStack(stack);
+  //
+  //   template.hasResourceProperties('AWS::S3::Bucket', {
+  //     BucketEncryption: {
+  //       ServerSideEncryptionConfiguration: [{
+  //         ServerSideEncryptionByDefault: {
+  //           SSEAlgorithm: 'AES256',
+  //         },
+  //       }],
+  //     },
+  //   });
+  // });
 });
